520fa00b1c327273d2b23669bce7e56f8bf9c012
Karim Asaad <KarimAsaad798/github.com>
2022-01-09 01:25:51 +0200

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { JobsComponent } from './jobs/jobs.component';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:HeaderComponent},
+  {path:"jobs" , component:JobsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
